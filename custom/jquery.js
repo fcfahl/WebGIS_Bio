668c87b50210ecfc62c4c3c6ac4c17df59cb8253
fcfahl <fcfahl@gmx.com>
2016-02-28 13:33:47 +0100

    custom wms2

--- custom/jquery.js
+++ custom/jquery.js
@@ -197,13 +197,145 @@ $( document ).ready(function() {
     		transparent: true,
     		version: '1.3.0',
     		tiled:true,
-           srs:"EPSG:4326"
+            srs:"EPSG:4326"
        });
 
         map.addLayer(CustomWMS);
     });
 
 
+    // var Custom_host = 'http://wms.pcn.minambiente.it/ogc?map=/ms_ogc/WMS_v1.3/Vettoriali/Carta_geologica.map&';
 
+    // http://wms.pcn.minambiente.it/ogc?map=/ms_ogc/WMS_v1.3/Vettoriali/Carta_geologica.map&service=wms&request=getCapabilities&version=1.3.0
 
+    // var Custom_host = 'http://wms.pcn.minambiente.it/ogc?map=/ms_ogc/WMS_v1.3/servizi-LiDAR/LIDAR_VALLE_DAOSTA.map&service=wms&request=getCapabilities&version=1.3.0';
+
+// var Custom_host = "http://wms.pcn.minambiente.it/ogc?map=/ms_ogc/WMS_v1.3/Vettoriali/Pericolosita_sismica_005.map&service=wms&request=getCapabilities&version=1.3.0"
+
+// var Custom_host = "http://wms.pcn.minambiente.it/ogc?map=/ms_ogc/WMS_v1.3/Vettoriali/Zone_sismogenetiche_ZS9.map&service=wms&request=getCapabilities&version=1.3.0"
+
+// var Custom_host = "http://metaspatial.net/cgi-bin/ogc-wms.xml?VERSION=1.3.0&REQUEST=GetMap&SERVICE=WMS&LAYERS=DTM,Overview,Raster_250K,Topography,nationalparks,Infrastructure,Places&STYLES=,,,,,,&CRS=EPSG:27700&BBOX=424735.97883597884,96026.98412698413,467064.02116402116,127773.01587301587&WIDTH=400&HEIGHT=300&FORMAT=image/png&BGCOLOR=0xffffff&TRANSPARENT=TRUE"
+//
+//     var parser = new ol.format.WMSCapabilities();
+//
+//     $.ajax(Custom_host).then(function(response) {
+//       var result = parser.read(response);
+//       var jons = window.JSON.stringify(result, null, 2)
+//     //   console.log(jons);
+//     //   $('#custom').html(window.JSON.stringify(result, null, 2));
+//
+// });
+//
+//     $.ajax({
+//             type: "GET",
+//     		url: Custom_host,
+//     		dataType: "xml",
+//     		success: function(xml) {
+//      			$(xml).find('Layer').each(function(){
+//      				if ($(this).children("Name").text() != null) {
+//                         console.log($(this).children("Name").text() )
+//      				// 	$("#custom").append('<option value="' + $(this).children("Name").text() + '">' + $(this).children("Title").text() + '</option>');
+//      				}
+//     			});
+//     		}
+//     	});
+
+        //
+
+        // function loadDoc() {
+        //     var xhttp = new XMLHttpRequest();
+        //     xhttp.onreadystatechange = function() {
+        //     if (xhttp.readyState == 4 && xhttp.status == 200) {
+        //       document.getElementById("demo").innerHTML = xhttp.responseText;
+        //     }
+        //     };
+        //     xhttp.open("GET", "demo_get.asp", true);
+        //     xhttp.send();
+        //     }
+
+        //
+        // $.get( Custom_host, function( data ) {
+        //     //   $( ".result" ).html( data );
+        //       console.log(data);
+        //     });
+
+        var Custom_host = "http://wms.pcn.minambiente.it/ogc?map=/ms_ogc/WMS_v1.3/Vettoriali/Zone_sismogenetiche_ZS9.map&service=wms&request=getCapabilities&version=1.3.0"
+
+            // https://api.jquery.com/jquery.get/
+            // var jqxhr = $.get( Custom_host, function( data, status, headers ) {
+            //     //   console.log( "success" );
+            //     })
+            //       .done(function(data, status, headers) {
+            //         console.log( "data" );
+            //         console.log( data);
+            //       })
+            //       .fail(function() {
+            //         console.log( "error" );
+            //       })
+            //       .always(function() {
+            //         console.log( "finished" );
+            //       });
+
+                var Custom_host = "http://localhost:8080/geoserver/wms?service=wms&version=1.1.1&request=GetCapabilities"
+
+              // http://fuzzytolerance.info/blog/2012/03/06/2012-03-06-parsing-wms-getcapabilities-with-jquery/
+            //   $.ajax({
+            //         type: "GET",
+            //   		url: Custom_host,
+            //   		dataType: "xml",
+            //   		success: function(xml) {
+            //    			$(xml).find('Layer').each(function(){
+            //    				if ($(this).children("Name").text() != null) {
+            //                     var name = $(this).children("Title").text();
+            //                     console.log( name);
+            //    					$(".list-group").append('<li class="list-group-item">' + $(this).children("Title").text() + '</li>');
+            //    				}
+            //   			});
+            //   		}
+            //   	});
+
+                $.ajax({
+                    type: "GET",
+                    url: Custom_host,
+                    dataType: "xml",
+                    success: parseXml
+                });
+
+                function parseXml(xml){
+                    $(xml).find("Capability").each(function(){
+                        var name = $(this).text();
+                        console.log( name);
+                        });
+                     };
+
+        //     function loadDoc() {
+        //         var formatter = new  ol.format.WMSCapabilities();
+        //         var endpoint = Custom_host;
+        //         var layers = [];
+        //         var http = new XMLHttpRequest();
+        //       // async call to geoserver (I'm using angular)
+        //     //   $http.get(endpoint);
+        //
+        //       http.open("GET", Custom_host, true);
+        //
+        //       success(function(data, status, headers, config) {
+        //
+        //           // use the tool to parse the data
+        //           var response = (formatter.read(data));
+        //
+        //           // this object contains all the GetCapabilities data
+        //           var capability = response.capability;
+        //
+        //           // I want a list of names to use in my queries
+        //           for(var i = 0; i < capability.layers.length; i ++){
+        //               layers.push(capability.layers[i].name);
+        //           }
+        //       }).
+        //
+        //       error(function(data, status, headers, config) {
+        //           alert("terrible error logging..");
+        //       });
+        //
+        // }
+        //     loadDoc()
 });
